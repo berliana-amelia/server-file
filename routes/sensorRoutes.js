6de77a151cdaa91b14db7454f1abaf22b9e61971
berliana-amelia <berliana.amelia.20@student.it.ittelkom-sby.ac.id>
2024-02-17 16:33:14 +0700

    Revert "update last online based on last get"
    This reverts commit 2d866fec9a39fd57e2ecf9c1df11de7dbd13e8bc.

--- routes/sensorRoutes.js
+++ routes/sensorRoutes.js
@@ -207,9 +207,7 @@ router.get("/", authenticateToken, async (req, res) => {
 
     // Update motor and sprayer status based on startStatus
     await updateMotorAndSprayerStatus(inorwat);
-    inorwat.lastOnline = currentTime;
-    // Save the changes to the document after 15 minutes
-    await inorwat.save();
+
     res.json({
       message: "Data accessed successfully",
       motor: inorwat.motor,
@@ -221,6 +219,7 @@ router.get("/", authenticateToken, async (req, res) => {
       endDate: inorwat.endDate,
       startDate: inorwat.startDate,
       lastOnline: inorwat.lastOnline,
+      currentTime: currentTime,
     });
   } catch (error) {
     res.status(500).send(error.message);
